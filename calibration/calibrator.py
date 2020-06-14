fix(calibration): All computation can work (but may not be correct)

calibration/calibrator.py
@@ -7,7 +7,7 @@ from detection.keypoint.utils import orientation_to_keypoints
 
 
 def compute_focus_region_mid_point():
-    return 1
+    return np.array([1296, 1024]).reshape(2, 1).astype(np.float)
 
 
 class Calibration:
@@ -44,7 +44,6 @@ class Calibration:
         calibs = filters.orientation_filter(calibs, 0.75)
 
         # compute average R
-        # z_avg = utils.compute_z_avg(calibs)
         r_avg = utils.compute_average_rotation_matrix(calibs)
         calibs[:, :, :3] = r_avg
 

calibration/camera.py
@@ -194,7 +194,7 @@ class Camera:
         else:
             warn('world_to_image not implemented')
 
-    def image_to_world(self, image_px, z):
+    def image_to_world(self, image_px, z, undistort=True):
         """
         Project image points with defined world z to world coordinates.
 
@@ -202,14 +202,16 @@ class Camera:
         :type image_px: numpy.array, shape=(2 or 3, n)
         :param z: world z coordinate of the projected image points
         :type z: float
+        :param undistort: do undistortion
+        :type undistort: bool
         :return: n projective world coordinates
         :rtype: numpy.array, shape=(3, n)
         """
         if image_px.shape[0] == 3:
             image_px = p2e(image_px)
-        image_undistorted = self.undistort(image_px)
+        image_undistorted = self.undistort(image_px) if undistort else image_px
         tmpP = np.hstack(
             (self.P[:, [0, 1]],
              self.P[:, 2, np.newaxis] * z + self.P[:, 3, np.newaxis]))
-        world_xy = p2e(np.linalg.inv(tmpP).dot(e2p(image_undistorted)))
+        world_xy = p2e(np.linalg.pinv(tmpP).dot(e2p(image_undistorted)))
         return np.vstack((world_xy, z * np.ones(image_px.shape[1])))

calibration/utils.py
@@ -36,22 +36,32 @@ def compute_average_rotation_matrix(calibs):
         rotated_vectors.append(rotated_vector / np.linalg.norm(rotated_vector))
     mean_rotated_vector = np.mean(np.stack(rotated_vectors), axis=0)
     mean_rotated_vector /= np.linalg.norm(mean_rotated_vector)
-    # compute average rotation matrix
+    # compute average rotation vector
     axis = np.cross(mean_rotated_vector, common_unit_vector)
     angle = np.dot(mean_rotated_vector, common_unit_vector)
     rvec = angle * axis
-    rmat = cv2.Rodrigues(rvec)[0]
+    # get basis with z_avg
+    z_avg = compute_z_avg(calibs)
+    basis_1, basis_2, _ = basis(z_avg)
+    # get representation of rvec under new basis
+    bases = np.stack([basis_1, basis_2, z_avg],
+                     axis=1)
+    rvec_repr = np.linalg.inv(bases) * np.eye(3) * rvec
+    rvec_new = rvec_repr[:, 0] * basis_1 + rvec_repr[:, 1] * basis_2 + rvec_repr[:, 2] * z_avg
+    # transform rvec to rotation matrix
+    rmat = cv2.Rodrigues(rvec_new)[0]
     return rmat
 
 
 def reproject_to_ground(p, calib, camera):
-    camera.set_R(calib[:, :3])
-    camera.set_t(calib[:, 3])
+    camera.R = calib[:, :3]
+    camera.t = calib[:, 3].reshape(3, 1)
+    camera.update_P()
     return camera.image_to_world(p, z=0)
 
 
 def distance_to_camera(p, calib):
-    point_to_camera = p + calib[:2, :]
+    point_to_camera = p + calib[:, 3]
     return np.linalg.norm(point_to_camera[:2])
 
 

tests/test_calibrator.py
@@ -18,16 +18,18 @@ model = KeypointDetector(checkpoint_path=CHECKPOINT_PATH,
 
 
 class TestCalibrator(unittest.TestCase):
-    def test_inference_from_bboxes(self):
+    def test_calibration(self):
         detections = object_detector.detect(frame=object_image)
         frame_rgb = cv2.cvtColor(object_image, cv2.COLOR_BGR2RGB)
         keypoints, orientations = model.detect(frame_rgb,
                                                detections[0],
                                                visualize=True)
         calibrator = Calibration()
-        calibrator.camera.set_K(np.array([[1, 0, 1], [0, 1, 1], [0, 0, 1]]))
+        calibrator.camera.set_K(np.array([[1, 0, 1296], [0, 1, 1024], [0, 0, 1]]))
         calibs = calibrator.calibrate(keypoints.cpu().numpy(),
                                       orientations.cpu().numpy())
+        calib_est = calibrator.filter_and_average(calibs)
+        print(calib_est)
 
 
 if __name__ == '__main__':